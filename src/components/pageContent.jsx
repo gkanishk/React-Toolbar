582b579241ec142581e0a14554bff28781cb8276
Kanishk gupta <kanishkgupta1234@gmail.com>
2019-06-23 01:02:07 +0530

    Added home

--- src/components/pageContent.jsx
+++ src/components/pageContent.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Image from 'react-bootstrap/Image';
-import iamges from './assests/image.jpg';
-const PageContent=props=>(
-    
-    // <div></div>
-    <Image src={iamges} fluid />
-);
+import iamges from './assests/img.jpg';
+import ListGroup from 'react-bootstrap/ListGroup'
+class PageContent extends Component{
+    state={
+        listState:false,
+        text:"Show Task"
+    }
+    showListItem=()=>{
+        this.setState({listState:true,text:"Hide Task"});
+    }
+    hideListItem=()=>{
+        this.setState({listState:false,text:"Show Task"});
+    }
+    render()
+    {
+        let listItems;
+        let changeButton;
+        changeButton=<button onClick={this.showListItem} >{this.state.text}</button>
+        if(this.state.listState)
+        {
+            listItems=<ListGroup variant="flush">
+            <ListGroup.Item>1. Make Proper Cart App</ListGroup.Item>
+            <ListGroup.Item>2. Show/Hide toggle using React</ListGroup.Item>
+            <ListGroup.Item>3. Facebook Notification button</ListGroup.Item>
+            <ListGroup.Item>4. Read All Mailbox</ListGroup.Item>
+            <ListGroup.Item>5. Tabs on React</ListGroup.Item>
+          </ListGroup>;
+          changeButton=<button onClick={this.hideListItem} >{this.state.text}</button>
+        }
+        if(this.state.listState===false)
+        {
+            listItems="";
+        }
+
+
+        return (
+    <div>
+   <Image src={iamges} fluid />
+   {changeButton}
+   {listItems}
+  </div>);
+    };
+}
 export default PageContent;
